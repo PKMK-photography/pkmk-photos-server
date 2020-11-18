5f8bb98e5c0c0d147f3884fb3a0d04e9c83e7e5b
mattcbodily <mattcbodily@gmail.com>
2020-11-18 14:44:53 -0700

    added massive and express-session

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const express = require('express'),
       massive = require('massive'),
       session = require('express-session'),
       emailCtrl = require('./controllers/emailController'),
+      {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
       app = express();
 
 app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}
+}));
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db);
+    console.log('db connected');
+});
 
 //email endpoint
 app.post('/api/email', emailCtrl.email);
