8dd6b1fcfa5b3b9228e986604cfc6730d676deb7
mattcbodily <mattcbodily@gmail.com>
2020-12-17 17:50:47 -0700

    making heroku changes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const { email } = require('./controllers/emailController');
-
 require('dotenv').config();
 const express = require('express'),
       emailCtrl = require('./controllers/emailController'),
+      port = process.env.PORT || 5555,
       app = express();
 
 app.use(express.json());
@@ -10,4 +9,35 @@ app.use(express.json());
 //email endpoint
 app.post('/api/email', emailCtrl.email);
 
-app.listen(5555, () => console.log('Server running on 5555'));
+const wakeUpDyno = (url, interval = 25, callback) => {
+    const milliseconds = interval * 60000;
+    setTimeout(() => {
+
+        try { 
+            console.log(`setTimeout called.`);
+            // HTTP GET request to the dyno's url
+            fetch(url).then(() => console.log(`Fetching ${url}.`)); 
+        }
+        catch (err) { // catch fetch errors
+            console.log(`Error fetching ${url}: ${err.message} 
+            Will try again in ${interval} minutes...`);
+        }
+        finally {
+
+            try {
+                callback(); // execute callback, if passed
+            }
+            catch (e) { // catch callback error
+                callback ? console.log("Callback failed: ", e.message) : null;
+            }
+            finally {
+                // do it all again
+                return wakeUpDyno(url, interval, callback);
+            }
+            
+        }
+
+    }, milliseconds);
+};
+
+app.listen(port, () => console.log(`Server running on ${port}`));
